dd3d930809477c4f65396bc2eaa234d0dc031f22
Henrique Bissoli Malaman Alonso <contato.henrique.bissoli@gmail.com>
2026-01-14 09:58:57 -0300

    feat/fix: Alteração na identificação e nome do app
    Foi alterada a identificação da aplicação de com.example.urna para ce156.sesisp.urna.
    Também foi alterado o script do GitHub Actions para gerar a assinatura do app ao gerar a build para Windows

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.urna"
+    namespace = "ce156.sesisp.urna"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.urna"
+        applicationId = "ce156.sesisp.urna"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
